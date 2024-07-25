1a687ae1e1517934893111a1116f51452a5eb4ef
Jose Manuel <j.ochoa317@gmail.com>
2024-07-25 18:08:47 -0500

    update: avoid memory leaks

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,15 +1,18 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, signal, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css'
 })
 export class CounterComponent {
   @Input({required: true}) duration = 0;
   @Input({required: true}) message = '';
+  counter = signal(0);
+  counterRef: number | undefined;
   
   constructor () {
     //no async
@@ -24,6 +27,10 @@ export class CounterComponent {
     console.log('ngOnChanges');
     console.log('-'.repeat(10));
     console.log(changes);
+    const duration = changes["duration"];
+    if(duration && duration.currentValue !== duration.previousValue){
+      this.doSomething();
+    }
   }
 
   ngOnInit(){
@@ -34,6 +41,10 @@ export class CounterComponent {
     console.log('-'.repeat(10));
     console.log("duration => ", this.duration);
     console.log("duration => ", this.message);
+    this.counterRef = window.setInterval(()=>{
+      console.log("run interval")
+      this.counter.update(statePrev => statePrev+1);
+    },1000)
   }
 
   ngAfterViewInit() {
@@ -46,5 +57,10 @@ export class CounterComponent {
   ngOnDestroy(){
     console.log("ngOnDestroy")
     console.log('-'.repeat(10));
+    window.clearInterval(this.counterRef);
+  }
+
+  doSomething(){
+    console.log("change duration");
   }
 }
